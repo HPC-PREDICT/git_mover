730488c003e20f440347083faa03493ab900fc70
Lukas Drescher <lukas.drescher@cscs.ch>
2021-12-20 15:09:23 +0100

    Adding support for pull requests (without reviews), issue comments, closed issues/PRs, adding metainformation to copied issues/PRs including a backlink to original ones, Python 3 print-statement fixes.

--- git-mover.py
+++ git-mover.py
@@ -1,12 +1,12 @@
-import requests, json, argparse, sys
+import requests, json, argparse, sys, datetime
 
 #Test if a response object is valid
 def check_res(r):
 	#if the response status code is a failure (outside of 200 range)
 	if r.status_code < 200 or r.status_code >= 300:
 		#print the status code and associated response. Return false
-		print "STATUS CODE: "+str(r.status_code)
-		print "ERROR MESSAGE: "+r.text
+		print("STATUS CODE: "+str(r.status_code))
+		print("ERROR MESSAGE: "+r.text)
 		#if error, return False
 		return False
 	#if successful, return True
@@ -50,8 +50,24 @@ INPUT:
 	source: the team and repo '<team>/<repo>' to retrieve issues from
 OUTPUT: retrieved issues sorted by their number if request was successful. False otherwise
 '''
+def download_pulls(source_url, source, credentials):
+	url = source_url+"repos/"+source+"/pulls?state=all&per_page=100"
+	r = get_req(url, credentials)
+	status = check_res(r)
+	if status:
+		#if the requests succeeded, sort the retireved issues by their number
+		sorted_pulls = sorted(json.loads(r.text), key=lambda k: k['number'])
+		return sorted_pulls
+	return False
+
+'''
+INPUT:
+	source_url: the root url for the GitHub API
+	source: the team and repo '<team>/<repo>' to retrieve issues from
+OUTPUT: retrieved issues sorted by their number if request was successful. False otherwise
+'''
 def download_issues(source_url, source, credentials):
-	url = source_url+"repos/"+source+"/issues?filter=all"
+	url = source_url+"repos/"+source+"/issues?state=all"
 	r = get_req(url, credentials)
 	status = check_res(r)
 	if status:
@@ -59,6 +75,26 @@ def download_issues(source_url, source, credentials):
 		sorted_issues = sorted(json.loads(r.text), key=lambda k: k['number'])
 		return sorted_issues
 	return False
+
+'''
+INPUT:
+	source_url: the root url for the GitHub API
+	source: the team and repo '<team>/<repo>' to retrieve issues from
+	issues: issues retrieved with download_issues
+OUTPUT: retrieved issue comments sorted by issue number and comment number if request was successful. False otherwise
+'''
+def download_issue_comments(source_url, source, credentials, issues):
+	issue_comments = []
+	for issue in issues:
+		url = source_url+"repos/"+source+"/issues/"+str(issue["number"])+"/comments?per_page=100" #FIXME: iterate over page=...
+		r = get_req(url, credentials)
+		status = check_res(r)
+		if status:
+			#if the requests succeeded, sort the retireved issues by their number
+			issue_comments.append(json.loads(r.text))
+		else:
+			issue_comments.append(None)
+	return issue_comments
 
 '''
 INPUT:
@@ -87,7 +123,10 @@ def create_milestones(milestones, destination_url, destination, credentials):
 	milestone_map = {}
 	for milestone in milestones:
 		#create a new milestone that includes only the attributes needed to create a new milestone
-		milestone_prime = {"title": milestone["title"], "state": milestone["state"], "description": milestone["description"], "due_on": milestone["due_on"]}
+		milestone_prime = {"title": milestone["title"], "state": milestone["state"], "description": milestone["description"]}
+		for k in ["description", "due_on"]:
+			if milestone[k] != None:
+				milestone_prime[k] = milestone[k]
 		r = post_req(url, json.dumps(milestone_prime), credentials)
 		status = check_res(r)
 		if status:
@@ -96,7 +135,7 @@ def create_milestones(milestones, destination_url, destination, credentials):
 			#map the original source milestone's number to the newly created milestone's number
 			milestone_map[milestone['number']] = returned_milestone['number']
 		else:
-			print status
+			print(status)
 	return milestone_map
 
 '''
@@ -124,7 +163,26 @@ def create_labels(labels, destination_url, destination, credentials):
 
 '''
 INPUT:
+	element_name: str, one of "issue", "pull_request" or "comment"
+	element: the corresponding element previously fetched from the source repo through the Github API
+OUTPUT: The top line of the element (prepended to body in POST requests)
+'''
+def get_element_header_line(element_name, element):
+	termination = "[" + element["user"]["login"] + "](" + element["user"]["html_url"] + ") on " + \
+				datetime.datetime.strptime(element['created_at'], "%Y-%m-%dT%H:%M:%SZ").strftime('%d %b %Y at %H:%M GMT') + \
+				"_\n\n"
+	if element_name != "comment":
+		return "_[Original " + element_name + " #" + str(element["number"]) + "](" + element["html_url"] + ")" \
+			   " created by " + termination
+	else:
+		return "_By " + termination
+
+
+'''
+INPUT:
+	pulls: python list of dicts containing pull request info to be POSTED to GitHub
 	issues: python list of dicts containing issue info to be POSTED to GitHub
+	issue_comments: python list of dicts containing issue comments to be POSTED to Github
 	destination_url: the root url for the GitHub API
 	destination_urlination: the team and repo '<team>/<repo>' to post issues to
 	milestones: a boolean flag indicating that milestones were included in this migration
@@ -132,34 +190,73 @@ INPUT:
 OUTCOME: Post issues to GitHub
 OUTPUT: Null
 '''
-def create_issues(issues, destination_url, destination, milestones, labels, milestone_map, credentials, sameInstall):
+def create_pulls_issues(pulls, issues, issue_comments, destination_url, destination, milestones, labels, milestone_map, credentials, sameInstall):
+	pull_requests = {pull["number"]: pull for pull in pulls}
+	pull_url = destination_url+"repos/"+destination+"/pulls"
 	url = destination_url+"repos/"+destination+"/issues"
-	for issue in issues:
-		#create a new issue object containing only the data necessary for the creation of a new issue
-		assignee = None
-		if (issue["assignee"] and sameInstall):
-			assignee = issue["assignee"]["login"]
-		issue_prime = {"title" : issue["title"], "body" : issue["body"], "assignee": assignee, "state" : issue["state"]}
-		#if milestones were migrated and the issue to be posted contains milestones
-		if milestones and "milestone" in issue and issue["milestone"]!= None:
-			#if the milestone associated with the issue is in the milestone map
-			if issue['milestone']['number'] in milestone_map:
-				#set the milestone value of the new issue to the updated number of the migrated milestone
-				issue_prime["milestone"] = milestone_map[issue["milestone"]["number"]]
-		#if labels were migrated and the issue to be migrated contains labels
-		if labels and "labels" in issue:
-			issue_prime["labels"] = issue["labels"]
-		r = post_req(url, json.dumps(issue_prime), credentials)
-		status = check_res(r)
-		#if adding the issue failed
-		if not status:
-			#get the message from the response
-			message = json.loads(r.text)
-			#if the error message is for an invalid entry because of the assignee field, remove it and repost with no assignee
-			if message['errors'][0]['code'] == 'invalid' and message['errors'][0]['field'] == 'assignee':
-				sys.stderr.write("WARNING: Assignee "+message['errors'][0]['value']+" on issue \""+issue_prime['title']+"\" does not exist in the destination repository. Issue added without assignee field.\n\n")
-				issue_prime.pop('assignee')
-				post_req(url, json.dumps(issue_prime), credentials)
+
+	for issue, comments in zip(issues, issue_comments):
+		# create issue or pull request
+		if "pull_request" not in issue:
+			#create a new issue object containing only the data necessary for the creation of a new issue
+			assignee = None
+			if (issue["assignee"] and sameInstall):
+				assignee = issue["assignee"]["login"]
+			issue_prime_body = get_element_header_line("issue", issue) + (issue["body"] if issue["body"] != None else "")
+			issue_prime = {"title" : issue["title"], "body" : issue_prime_body, "assignee": assignee}
+			#if milestones were migrated and the issue to be posted contains milestones
+			if milestones and "milestone" in issue and issue["milestone"]!= None:
+				#if the milestone associated with the issue is in the milestone map
+				if issue['milestone']['number'] in milestone_map:
+					#set the milestone value of the new issue to the updated number of the migrated milestone
+					issue_prime["milestone"] = milestone_map[issue["milestone"]["number"]]
+			#if labels were migrated and the issue to be migrated contains labels
+			if labels and "labels" in issue:
+				issue_prime["labels"] = issue["labels"]
+			r = post_req(url, json.dumps(issue_prime), credentials)
+			status = check_res(r)
+			#if adding the issue failed
+			if not status:
+				#get the message from the response
+				message = json.loads(r.text)
+				#if the error message is for an invalid entry because of the assignee field, remove it and repost with no assignee
+				if message['errors'][0]['code'] == 'invalid' and message['errors'][0]['field'] == 'assignee':
+					sys.stderr.write("WARNING: Assignee "+message['errors'][0]['value']+" on issue \""+issue_prime['title']+"\" does not exist in the destination repository. Issue added without assignee field.\n\n")
+					issue_prime.pop('assignee')
+					post_req(url, json.dumps(issue_prime), credentials)
+		else: # pull_request (basic version, could be extended)
+			pull_number = int(issue["pull_request"]["url"].split('/')[-1])
+			pull = pull_requests[pull_number]
+			# implemented only for same repo PRs (FIXME: for PRs from forks)
+			pull_prime_body = get_element_header_line("pull request", pull) + (pull["body"] if pull["body"] != None else "")
+			pull_prime = {"title" : pull["title"], "body" : pull_prime_body, "head" : pull["head"]["label"].split(":")[-1], "base" : pull["base"]["label"].split(":")[-1]}
+			r = post_req(pull_url, json.dumps(pull_prime), credentials)
+			status = check_res(r)
+			if not status:
+				# get the message from the response
+				message = json.loads(r.text)
+				sys.stderr.write("ERROR: Pull request " + str(pull["number"]) + " " + message)
+
+		# copy comments (reviews, etc. not covered currently)
+		issue_number = json.loads(r.text)["number"]
+		comment_url = url+"/"+str(issue_number)+"/comments"
+		for comment in comments:
+			comment_prime = {"body" : get_element_header_line("comment", comment) + comment["body"]}
+			comment_r = post_req(comment_url, json.dumps(comment_prime), credentials)
+			comment_status = check_res(comment_r)
+			if not comment_status:
+				sys.stderr.write("ERROR: Writing comment for issue " + str(issue_number) + ":" + json.loads(comment_r.text))
+
+		# final status update if closed (potentially separate function)
+		if issue["state"] == "closed":
+			issue_prime = {"state": "closed"}
+			r = post_req(url+"/"+str(issue_number) if "pull_request" not in issue else pull_url+"/"+str(issue_number),
+						 json.dumps(issue_prime), credentials)
+			status = check_res(r)
+			if not status:
+				# get the message from the response
+				message = json.loads(r.text)
+				sys.stderr.write("ERROR: Issue/Pull request status update " + str(issue_number) + " " + message)
 
 
 def main():
@@ -216,7 +313,7 @@ def main():
 			sys.stderr.write('ERROR: Milestones failed to be retrieved. Exiting...')
 			quit()
 		else:
-			print "No milestones found. None migrated"
+			print("No milestones found. None migrated")
 
 	if args.labels:
 		labels = download_labels(source_root, source_repo, source_credentials)
@@ -226,20 +323,22 @@ def main():
 			sys.stderr.write('ERROR: Labels failed to be retrieved. Exiting...')
 			quit()
 		else:
-			print "No Labels found. None migrated"
+			print("No Labels found. None migrated")
 
 	if args.issues:
+		pulls = download_pulls(source_root, source_repo, source_credentials)
 		issues = download_issues(source_root, source_repo, source_credentials)
+		issue_comments = download_issue_comments(source_root, source_repo, source_credentials, issues)
 		if issues:
 			sameInstall = False
 			if (args.sourceRoot == args.destinationRoot):
 				sameInstall = True
-			res = create_issues(issues, destination_root, destination_repo, args.milestones, args.labels, milestone_map, destination_credentials, sameInstall)
+			res = create_pulls_issues(pulls, issues, issue_comments, destination_root, destination_repo, args.milestones, args.labels, milestone_map, destination_credentials, sameInstall)
 		elif issues == False:
 			sys.stderr.write('ERROR: Issues failed to be retrieved. Exiting...')
 			quit()
 		else:
-			print "No Issues found. None migrated"
+			print("No Issues found. None migrated")
 
 if __name__ == "__main__":
 	main()
